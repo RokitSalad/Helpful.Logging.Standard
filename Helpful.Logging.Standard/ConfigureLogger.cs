f6f5a0dd8f30068ba4020f2af30c4bf0c8d362c4
rokitsalad <peterdobbs@hotmail.com>
2020-06-01 21:51:19 +1000

    added exception for failed configuration to allow consumers to know logging isn't working

--- Helpful.Logging.Standard/ConfigureLogger.cs
+++ Helpful.Logging.Standard/ConfigureLogger.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using Serilog;
 using Serilog.Formatting.Compact;
 
@@ -10,29 +9,36 @@ namespace Helpful.Logging.Standard
     {
         public static void StandardSetup(Action appInit = null, string logFileName = "log.txt")
         {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Information()
-                .WriteTo.Console()
-                .WriteTo.File(new CompactJsonFormatter(), logFileName, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
-                .Enrich.FromLogContext()
-                .CreateLogger();
-
-
-            var entryAssembly = Assembly.GetEntryAssembly();
-            var executingAssembly = entryAssembly?.FullName;
-            entryAssembly.GetLogger().LogInformationWithContext("Application {ApplicationName} is starting.", executingAssembly);
-
-            if (appInit != null)
+            try
             {
-                try
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Information()
+                    .WriteTo.Console()
+                    .WriteTo.File(new CompactJsonFormatter(), logFileName, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
+                    .Enrich.FromLogContext()
+                    .CreateLogger();
+
+
+                var entryAssembly = Assembly.GetEntryAssembly();
+                var executingAssembly = entryAssembly?.FullName;
+                entryAssembly.GetLogger().LogInformationWithContext("Application {ApplicationName} is starting.", executingAssembly);
+
+                if (appInit != null)
                 {
-                    appInit();
+                    try
+                    {
+                        appInit();
+                    }
+                    catch (Exception e)
+                    {
+                        entryAssembly.GetLogger().LogFatalWithContext(e, "The application {ApplicationName} failed to start due to an exception.", executingAssembly);
+                        throw;
+                    }
                 }
-                catch (Exception e)
-                {
-                    entryAssembly.GetLogger().LogFatalWithContext(e, "The application {ApplicationName} failed to start due to an exception.", executingAssembly);
-                    throw;
-                }
+            }
+            catch (Exception e)
+            {
+                throw new HelpfulLoggingConfigurationException("Failed to configure Helpful.Logging.Standard.", e);
             }
         }
     }
---
+++ Helpful.Logging.Standard/HelpfulLoggingConfigurationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Helpful.Logging.Standard
+{
+    public class HelpfulLoggingConfigurationException : Exception
+    {
+        public HelpfulLoggingConfigurationException(string message, Exception innerException) : base(message, innerException)
+        {
+            
+        }
+    }
+}
